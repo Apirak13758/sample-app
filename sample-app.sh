dd6c7973eeeae7626f76383ef90949c564b8f123
Apirak13758 <66070218@kmitl.ac.th>
2025-08-16 22:08:10 +0000

    Committing sample-app files.

---
+++ sample-app.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Setup
+rm -rf tempdir
+mkdir -p tempdir/templates tempdir/static
+
+# Copy files
+cp sample_app.py tempdir/
+[ -d templates ] && cp -r templates/* tempdir/templates/
+[ -d static ] && cp -r static/* tempdir/static/
+
+# Create Dockerfile
+cat <<EOF > tempdir/Dockerfile
+FROM python:3.10-slim
+WORKDIR /home/myapp
+RUN pip install --no-cache-dir --progress-bar=off --root-user-action=ignore flask
+COPY ./static ./static/
+COPY ./templates ./templates/
+COPY sample_app.py .
+EXPOSE 8080
+CMD ["python3", "sample_app.py"]
+EOF
+
+# Build and run
+cd tempdir || exit
+docker build -t sampleapp .
+docker run -t -d -p 8080:8080 --memory=1g --name samplerunning sampleapp
+docker ps -a
